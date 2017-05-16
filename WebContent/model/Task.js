ec414dcf985b03e4f47e6c95412db9cf417d4bd6
Raymond Bergholm <raymond.bergholm@capgemini.com>
2017-05-16 15:49:47 +0300

    fixed model data structures

--- WebContent/model/Task.js
+++ WebContent/model/Task.js
@@ -1,6 +1,13 @@
-function Task(id, title, description) {
+function Task(title, description) {
+    var taskId;
+    if(TaskManager){
+        taskId = TaskManager.getNextTaskId();
+    }else {
+        taskId = 0;
+    }
+
     return {
-        id: id || 0,
+        id: taskId,
         title: title || "",
         description: description || "",
         status: "none", //TaskStatus.None, // might be a race condition here, depends on whether TaskStatus has been initialized at the time of instantiation
