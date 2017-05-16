c43738c56ba4007525e5eb5a5d5140ae8a2b701c
Raymond Bergholm <raymond.bergholm@capgemini.com>
2017-05-16 11:15:19 +0300

    better Task constructor

--- WebContent/model/Task.js
+++ WebContent/model/Task.js
@@ -1,7 +1,9 @@
-function Task() {
-    this.id = 0;
-    this.title = "";
-    this.description = "";
-    this.status = TaskStatus.None;
-    this.comments = [];
+function Task(id, title, description) {
+    return {
+        id: id || 0,
+        title: title || "",
+        description: description || "",
+        status: "none", //TaskStatus.None, // might be a race condition here, depends on whether TaskStatus has been initialized at the time of instantiation
+        comments: []
+    };
 }
