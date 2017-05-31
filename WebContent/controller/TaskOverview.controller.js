890688515423f0f2cec8d3e44ec30ec8f99fcbea
Raymond Bergholm <raymond.bergholm@capgemini.com>
2017-05-31 16:07:28 +0300

    flattened view structure, moving to semantic page architecture

--- WebContent/controller/TaskOverview.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(BaseController){
-    "use strict";
-
-    return BaseController.extend("com.tasky.controller.TaskOverview", {
-        // Extend this controller as required
-    });
-});
