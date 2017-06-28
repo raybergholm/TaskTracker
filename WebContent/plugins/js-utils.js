5852fe1bdf693e60c0e8d10f78564f4722578433
raybergholm <raybergholm@gmail.com>
2017-06-28 16:48:04 +0300

    some missing spaces

--- WebContent/plugins/js-utils.js
+++ WebContent/plugins/js-utils.js
@@ -1,21 +1,21 @@
 var jsUtils = new JsUtils();;
 
-function JsUtils(){
+function JsUtils() {
     "use strict";
 
     this.Object = {
-        clone: function(source){
+        clone: function(source) {
             // NOTE: Object.assign() copies properties which could be accessed using .hasOwnProperty.
             // Variables and functions will be separate, but if a property is a reference,
             // the reference gets copied: this means that this clone behaviour is only one level deep,
             // anything deeper is still a reference to the original object. Also, since arrays are objects it will also be a reference here
 
             var destination;
-            if(source instanceof Array){
+            if(source instanceof Array) {
                 destination = Object.assign([], source);
-            }else if(typeof source === "object"){
+            } else if(typeof source === "object") {
                 destination = Object.assign({}, source);
-            }else{
+            } else {
                 console.error("Source for clone operation was not an object or array");
                 destination = null;
             }
@@ -23,21 +23,21 @@ function JsUtils(){
             return destination;
         },
 
-        deepClone: function(source){
+        deepClone: function(source) {
             var destination = {}; // FIXME: actually the fact arrays need special handling is just a giant PitA. Maybe split it to Array.deepClone?
 
-            if(typeof source !== "object"){
+            if(typeof source !== "object") {
                 console.error("Source for deep clone operation was not an object");
                 return null;
             }
 
-            for(var prop in source){
-                if(source.hasOwnProperty(prop)){
-                    if(prop instanceof Array){
+            for(var prop in source) {
+                if(source.hasOwnProperty(prop)) {
+                    if(prop instanceof Array) {
                         // FIXME: arrays are objects too, but this isn't being handled
-                    }else if(typeof prop === "object"){
+                    } else if(typeof prop === "object") {
                         destination[prop] = this.deepClone(source[prop]);
-                    }else{
+                    } else {
                         destination[prop] = source[prop];
                     }
                 }
@@ -77,9 +77,9 @@ function JsUtils(){
         //  format({}, ["hello"])           - object isn't a string (no replace function)
         //  format([], ["hello"]])          - array isn't a string (no replace function)
         //  format(123, ["hello"]])         - integer isn't a string (no replace function)
-        format: function(str, replacements){
-            return str.replace(/\{\d+\}/g, function(match){
-                return replacements[match.substring(1, match.length -1)] || match;
+        format: function(str, replacements) {
+            return str.replace(/\{\d+\}/g, function(match) {
+                return replacements[match.substring(1, match.length - 1)] || match;
             });
         }
     };
