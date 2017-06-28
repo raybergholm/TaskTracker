97c9d1c6400dcdba8f4eb9ef345e0bc5368f4086
raybergholm <raybergholm@gmail.com>
2017-06-28 16:41:50 +0300

    checked how JsUtils.String.format behaves when you feed in strange values, added comments on results

--- WebContent/plugins/js-utils.js
+++ WebContent/plugins/js-utils.js
@@ -48,6 +48,35 @@ function JsUtils(){
     };
 
     this.String = {
+        // Functions mostly like String.Format() in other languages.
+        //
+        // Normal use cases:
+        //  format("{0} {1}", ["Hello", "world"]) -> "Hello world"
+        //  format("hi {0}", ["Bob"]) -> "hi Bob"
+        //
+        // Weirder use cases:
+        //  format("{0} {1}", {0: "Hello", "1": "world"}) -> "Hello world"      - works because of how JS treats arrays, maps and objects
+        //  format("{0}{1}{4}{5}", "hello") -> "heo{5}"                         - I suppose it's legal to pass in a string and match chars by index. Maybe a bit weird though.
+        //
+        // Use cases with mismatches:
+        //  format("{0} {1}", ["gibberish"]) -> "gibberish {1}"     - {0} ok but {1} not matched
+        //  format("{2}", ["Hello", "world"]) -> "{2}"              - array doesn't contain that many elements
+        //  format("{0}", []) -> "{0}"                              - empty array, nothing will get matched
+        //  format("Hi there", ["Hello", "world"]) -> "Hi there"    - no match tokens: array doesn't matter then. String stays the same
+        //
+        // Results when you deliberately pass in wrong formats:
+        //  format("{0} {1}", {}) -> "{0} {1}"                      - empty object
+        //  format("{0} {1}", 123) -> "{0} {1}"                     - integer
+        //  format("{0}", [123]) -> "123"                           - array with integer
+        //  format("{0}", [{}]) -> "[object Object]"                - array with object object.toString() called
+        //  format({replace: function(){return "hi"}}, ["hello"])   - ... ok, that's just evil. Don't do this
+        //
+        // Stuff that throws an error:
+        //  format()                        - undefined string (and array)
+        //  format("{0}")                   - undefined array
+        //  format({}, ["hello"])           - object isn't a string (no replace function)
+        //  format([], ["hello"]])          - array isn't a string (no replace function)
+        //  format(123, ["hello"]])         - integer isn't a string (no replace function)
         format: function(str, replacements){
             return str.replace(/\{\d+\}/g, function(match){
                 return replacements[match.substring(1, match.length -1)] || match;
